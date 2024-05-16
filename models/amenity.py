e1cda3e9db464430c5f4bcb688010cc2e30c2ef3
machage9603 <mikemachage@gmail.com>
2024-05-16 23:41:21 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-"""Defines the amenity class"""
-
+"""Defines the Amenity class"""
 from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """
-    child class of BaseModel
-    has one attribute - name(empty string)
-    """
-    name = ""
-    
