5e2597f92e9cab191f565062929ea895ef2fc46f
machage9603 <mikemachage@gmail.com>
2024-05-19 23:54:26 +0300

    :white_check_mark: #6: Created console

--- console.py
+++ console.py
@@ -3,14 +3,14 @@
 """Defines the HBnB console"""
 
 import cmd
-import models
-from models.base_model import BaseModel
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
+from . import models
+from . models.base_model import BaseModel
+from . models.state import State
+from . models.city import City
+from . models.amenity import Amenity
+from . models.place import Place
+from . models.review import Review
+from . models.user import User
 from datetime import datetime
 from shlex import shlex
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,8 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.review import Review   
+from models.review import Review
+import os  
 
 
 class FileStorage:
