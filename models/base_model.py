5a796a8a00d90d4482261be494fd425ac3bc8adb
machage9603 <mikemachage@gmail.com>
2024-05-20 02:11:28 +0300

    :white_check_mark: #12: Solving circular import issue

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@
 
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -34,6 +33,7 @@ class BaseModel:
 
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime."""
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
